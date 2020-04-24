c93c25496216e356bf7c42c2ddfaa0e222ed0efd
Ubuntu <ubuntu@ip-172-31-47-168.us-west-2.compute.internal>
2020-04-24 15:23:38 +0000

    changing indentation and expanding try statement in log_extractor.py

--- results/log_extractor.py
+++ results/log_extractor.py
@@ -16,27 +16,26 @@ folder_list = list(os.listdir())
 multiple_store_list = []
 
 for folder in folder_list:
-	os.chdir(folder)
-	store_id_list = [d for d in os.listdir() if os.path.isdir(d)]
-	if len(store_id_list) > 1: 
-		multiple_store_list.append(folder)
-	try:
-		s = Store(os.getcwd(), os.getcwd()+"/"+store_id_list[0])
-	except:
-		os.chdir("..")
-	else:
-		new_df = s['logs'].df
+    os.chdir(folder)
+    store_id_list = [d for d in os.listdir() if os.path.isdir(d)]
+    if len(store_id_list) > 1: 
+        multiple_store_list.append(folder)
+    try:
+        s = Store(os.getcwd(), os.getcwd()+"/"+store_id_list[0])
+        new_df = s['logs'].df
+    except:
+        os.chdir("..")
+    else:
+        new_df['source_eps'] = re.findall(r'source_eps_([a-z,0-9]+)', folder)[0]
+        new_df['target_ds'] = re.findall(r'target_dataset_([a-z,0-9]+)', folder)[0]
+        new_df['num_training_images'] = re.findall(r'num_training_images_([a-z,0-9,-]+)', folder)[0]
+        new_df['unfrozen_blocks'] = re.findall(r'unfrozen_blocks_([a-z,0-9]+)', folder)[0]
+        new_df['seed'] = re.findall(r'seed_([a-z,0-9]+)', folder)[0]
 
-		new_df['source_eps'] = re.findall(r'source_eps_([a-z,0-9]+)', folder)[0]
-		new_df['target_ds'] = re.findall(r'target_dataset_([a-z,0-9]+)', folder)[0]
-		new_df['num_training_images'] = re.findall(r'num_training_images_([a-z,0-9,-]+)', folder)[0]
-		new_df['unfrozen_blocks'] = re.findall(r'unfrozen_blocks_([a-z,0-9]+)', folder)[0]
-		new_df['seed'] = re.findall(r'seed_([a-z,0-9]+)', folder)[0]
+        df = df.append(new_df)
+        s.close()
 
-		df = df.append(new_df)
-		s.close()
-
-		os.chdir("..")
+        os.chdir("..")
 
 os.chdir("..")
 df.to_csv(args.o)
