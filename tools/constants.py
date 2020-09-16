78d19a73333af94584d57e79f631aceeab5f6784
Francisco Utrera <francisco.utrera@outlook.com>
2020-09-16 03:10:54 -0700

    new files

---
+++ tools/constants.py
@@ -0,0 +1,14 @@
+
+eps_to_filename = {
+        -1:    None, # Do not load a model that has been trained from scratch
+        0:    'nat',
+	0.05: 'imagenet_l2_0_05.pt',
+	0.25: 'imagenet_l2_0_25.pt',
+	1:    'imagenet_l2_1_0.pt',
+	3:    'imagenet_l2_3_0.pt',
+	4:    'imagenet_linf_4.pt',
+	8:    'imagenet_linf_8.pt'
+}
+
+data_path = '/scratch/data/'
+
---
+++ tools/transforms.py
@@ -0,0 +1,48 @@
+from torchvision import transforms
+
+# DOWNSCALE RESOLUTION TRANSFORMS
+
+TRAIN_TRANSFORMS_DOWNSCALE = lambda downscale, upscale: transforms.Compose([
+            transforms.Resize(downscale),
+            transforms.Resize(upscale),
+            transforms.RandomCrop(upscale, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(.25,.25,.25),
+            transforms.RandomRotation(2),
+            transforms.ToTensor(),
+        ])
+
+TEST_TRANSFORMS_DOWNSCALE = lambda downscale, upscale:transforms.Compose([
+        transforms.Resize(downscale),
+        transforms.Resize(upscale),
+        transforms.CenterCrop(upscale),
+        transforms.ToTensor()
+    ])
+
+
+# DEFAULT TRANSFORMS
+
+TRAIN_TRANSFORMS_DEFAULT = lambda size: transforms.Compose([
+            transforms.Resize(size),
+            transforms.RandomCrop(size, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ColorJitter(.25,.25,.25),
+            transforms.RandomRotation(2),
+            transforms.ToTensor(),
+        ])
+
+TEST_TRANSFORMS_DEFAULT = lambda size:transforms.Compose([
+        transforms.Resize(size),
+        transforms.CenterCrop(size),
+        transforms.ToTensor()
+        ])
+
+# MNIST TRANSFORMS
+
+TRANSFORM_MNIST = lambda size: transforms.Compose([
+        transforms.Resize(size),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+        ])
+
+
